32512f9cba02045e9aca05c0be55f7b4df5095a5
MaximShavva <saloedos@yandex.ru>
2019-03-17 15:08:11 +0300

    1.4. JUnit[#121794]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,12 +8,21 @@ package ru.job4j.calculate;
 */
 public class Calculate {
 
-/**
-* Метод выводит в консоль строку "Hello World!"
-* @param args принимает набор аргументов строкового
-* типа из строки запуска программы.
-*/	
+	/**
+	* Метод выводит в консоль строку "Hello World!"
+	* @param args принимает набор аргументов строкового
+	* типа из строки запуска программы.
+	*/	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
+	
+	/**
+     * Method echo.
+     * @param name Ваше имя.
+     * @return Эхо, эхо, эхо и ваше имя.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate class test.
+*
+* @author Шавва Максим (masyam@mail.ru)
+* @version 1
+* @since 0.1
+*/
+package ru.job4j.calculate;
