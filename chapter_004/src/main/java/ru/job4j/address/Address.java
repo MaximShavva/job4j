ad32a6c6e322f077786e65ec7588285edd1eaeef
MaximShavva <saloedos@yandex.ru>
2019-04-25 19:49:20 +0300

    3. Уникальность элементов и сортировка.[#121716]

---
+++ chapter_004/src/main/java/ru/job4j/address/Address.java
@@ -0,0 +1,73 @@
+package ru.job4j.address;
+
+import java.util.Objects;
+
+/**
+ * Класс содержит данные адреса клаента.
+ *
+ * @author Шавва Максим.
+ * @version 1.
+ * @since 25.04.2019г.
+ */
+public class Address {
+
+    /**
+     * Город.
+     */
+    private String city;
+
+    /**
+     * Улица.
+     */
+    private String street;
+
+    /**
+     * Дом.
+     */
+    private int home;
+
+    /**
+     * Квартира.
+     */
+    private int apartment;
+
+    public Address(String city, String street, int home, int apartment) {
+        this.city = city;
+        this.street = street;
+        this.home = home;
+        this.apartment = apartment;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return home == address.home
+                && apartment == address.apartment
+                && Objects.equals(city, address.city)
+                && Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{"
+                + "city='" + city + '\''
+                + ", street='" + street + '\''
+                + ", home=" + home
+                + ", apartment=" + apartment + '}';
+    }
+}
---
+++ chapter_004/src/main/java/ru/job4j/address/Profiles.java
@@ -0,0 +1,29 @@
+package ru.job4j.address;
+
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Класс содержит метод collect() для выборки адресов из списка порфилей.
+ * Так же метод убирает одинаковые адреса и сортирует адреса по городам.
+ *
+ * @author Шавва Максим.
+ * @version 1.
+ * @since 25.04.2019г.
+ */
+public class Profiles {
+
+    /**
+     * @param profiles Список всех профилей клиентов.
+     * @return Список адресов клиентов.
+     */
+    List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .distinct()
+                .sorted(Comparator.comparing(Address::getCity))
+                .collect(Collectors.toList());
+    }
+}
